b05ed1a876ff170dab0a1ee60a38427a1f543f1a
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-11-12 09:48:08 +0200

    Initial project setup

---
+++ NamespacesExercises/NamespacesExercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NamespacesExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
